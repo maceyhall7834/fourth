fa90b3ce7a255867de209e7b8c36dc4f659ef323
maceyhall7834 <maceyhall7834@outlook.com>
2026-03-03 02:05:29 +0600

    Update shard.js

--- shard.js
+++ shard.js
@@ -1,15 +1,13 @@
 // shard.js
 
 const Eris = require("eris");
-const Constants = require("eris").Constants; // Import from the public API
 
 module.exports = function patchShard(Shard) {
   Shard.prototype.identify = function () {
     this.status = "identifying";
-    
     const identify = {
       token: this._token,
-      v: Constants.GATEWAY_VERSION, // Use GATEWAY_VERSION from Constants
+      v: Eris.Constants.GATEWAY_VERSION,
       compress: !!this.client.options.compress,
       large_threshold: this.client.options.largeThreshold,
       intents: this.client.options.intents,
@@ -19,14 +17,12 @@ module.exports = function patchShard(Shard) {
         device: "mobile",
       },
     };
-
     if (this.client.options.maxShards > 1) {
       identify.shard = [this.id, this.client.options.maxShards];
     }
     if (this.presence.status) {
       identify.presence = this.presence;
     }
-    
-    this.sendWS(Constants.GatewayOPCodes.IDENTIFY, identify); // Use GatewayOPCodes from Constants
+    this.sendWS(Eris.Constants.GatewayOPCodes.IDENTIFY, identify);
   };
 };
