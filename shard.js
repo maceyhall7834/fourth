616feb8b3cf527a77e8a7e12d8a8d27718404b56
maceyhall7834 <maceyhall7834@outlook.com>
2026-03-03 02:15:31 +0600

    Patch Shard.identify for mobile device properties
    Patch Eris's Shard.identify method to treat the connection as mobile by overriding default properties.

--- shard.js
+++ shard.js
@@ -1,31 +1,37 @@
 // shard.js
+// patch Eris's Shard.identify method so Discord treats the connection as mobile
+// (overrides default "Eris"/"web" fields). This patch is applied when
+// the module is loaded.
 
-const Eris = require("eris");
+module.exports = function patchShard() {
+  try {
+    const Constants = require('eris/lib/Constants');
+    const { GATEWAY_VERSION, GatewayOPCodes } = Constants;
+    const Shard = require('eris/lib/gateway/Shard');
 
-module.exports = function patchShard(Shard) {
-  const { GATEWAY_VERSION, GatewayOPCodes } = Eris.Constants;  // Access constants directly
-
-  Shard.prototype.identify = function () {
-    this.status = "identifying";
-    const identify = {
-      token: this._token,
-      v: GATEWAY_VERSION,
-      compress: !!this.client.options.compress,
-      large_threshold: this.client.options.largeThreshold,
-      intents: this.client.options.intents,
-      properties: {
-        os: "Android",
-        browser: "Discord Android",
-        device: "mobile",
-      },
+    Shard.prototype.identify = function () {
+      // same logic as upstream, but with mobile device properties
+      this.status = "identifying";
+      const identify = {
+        token: this._token,
+        v: GATEWAY_VERSION,
+        compress: !!this.client.options.compress,
+        large_threshold: this.client.options.largeThreshold,
+        intents: this.client.options.intents,
+        properties: {
+          os: "Android",
+          browser: "Discord Android",
+          device: "mobile",
+        },
+      };
+      if (this.client.options.maxShards > 1) {
+        identify.shard = [this.id, this.client.options.maxShards];
+      }
+      if (this.presence.status) identify.presence = this.presence;
+      this.sendWS(GatewayOPCodes.IDENTIFY, identify);
     };
-    if (this.client.options.maxShards > 1) {
-      identify.shard = [this.id, this.client.options.maxShards];
-    }
-    if (this.presence.status) {
-      identify.presence = this.presence;
-    }
-    
-    this.sendWS(GatewayOPCodes.IDENTIFY, identify);  // This line will work correctly
-  };
+  } catch (e) {
+    // if Eris internals change or module can't be loaded, warn and continue
+    console.warn('Could not patch Shard.identify for mobile device', e);
+  }
 };
